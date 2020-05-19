d4ea0181d8d83a44fad5474a971b66f2a47751da
Bowie <bowie@thinkful.com>
2020-05-19 09:15:01 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function jumpPage() {
+    $('.nav-wrap li').click(function() {
+        let id = "#" + $(this).attr('goto');
+        let top = $(id).position().top;
+        $('html').scrollTop(top);
+    });
+}
+
+function dropMenuJump() {
+    $('.dropMenu li').click(function() {
+        let id = "#" + $(this).attr('goto');
+        let top = $(id).position().top;
+        $('html').scrollTop(top);
+    });
+}
+
+dropMenuJump();
+jumpPage();
+// openNewTab();
