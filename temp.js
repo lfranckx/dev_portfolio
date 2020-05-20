6a1fbd64d7cfb6030c2b8d4526a117c278b4c45c
Bowie <bowie@thinkful.com>
2020-05-20 00:36:45 -0700

    media queries

--- temp.js
+++
@@ -1,9 +0,0 @@
-function showPage() {
-    $('.nav-list li').click(function() {
-        let id = ('#') + $(this).attr('show');
-        console.log(id);
-        $(id).toggleClass("hidden");
-    });
-}
-
-showPage();
