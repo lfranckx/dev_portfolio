089bc3ae78ebbd6fcc8be1b6c01014b1eff1c9fc
Bowie <bowie@thinkful.com>
2020-05-29 14:11:57 -0700

    add scrolling speed

--- index.js
+++ index.js
@@ -2,18 +2,8 @@ function jumpPage() {
     $('.nav-wrap li').click(function() {
         let id = "#" + $(this).attr('goto');
         let top = $(id).position().top;
-        $('html').scrollTop(top);
+        $('html').animate({ scrollTop: top }, 500);
     });
 }
 
-function dropMenuJump() {
-    $('.dropMenu li').click(function() {
-        let id = "#" + $(this).attr('goto');
-        let top = $(id).position().top;
-        $('html').scrollTop(top);
-    });
-}
-
-dropMenuJump();
 jumpPage();
-openNewTab();
